f263a7343a178c4da69c21b0a2f8ddefd3651de7
sangram <balsangram1@gmail.com>
2025-07-13 00:02:42 +0530

    small update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useRoutes } from 'react-router-dom';
 import Router from './routes/Router';
 
 function App() {
+  console.log("update");
+  
   const routing = useRoutes(Router);
   return routing;
 }
